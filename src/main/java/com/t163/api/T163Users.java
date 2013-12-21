40d74bb135a27f388b6a802d07a5e84c674c9300
cuizuoli <cuizuoli@cuizuoli-PC>
2013-12-21 15:29:29 +0800

    added users/show api.

--- src/main/java/com/t163/api/T163Users.java
+++ src/main/java/com/t163/api/T163Users.java
@@ -12,6 +12,7 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Component;
 
 import com.t163.http.client.T163HttpClient;
+import com.t163.model.T163User;
 
 /**
  * nhn t-163-api
@@ -22,9 +23,24 @@ import com.t163.http.client.T163HttpClient;
 @Component
 public class T163Users {
 
-	private static final String USERS_SHOW_URL = "https://api.weibo.com/2/users/show.json";
+	private static final String USERS_SHOW_URL = "https://api.t.163.com/users/show.json";
 
 	@Resource
 	private T163HttpClient t163HttpClient;
+
+	/**
+	 * http://open.t.163.com/wiki/index.php?title=%E8%8E%B7%E5%8F%96%E6%8C%87%E5%AE%9A%E7%94%A8%E6%88%B7%E7%9A%84%E4%BF%A1%E6%81%AF(users/show)
+	 * @param userId
+	 * @param accessToken
+	 * @return
+	 */
+	public T163User show(String userId, String accessToken) {
+		String url = new StringBuffer()
+			.append(USERS_SHOW_URL)
+			.append("?user_id=").append(userId)
+			.append("&access_token=").append(accessToken)
+			.toString();
+		return t163HttpClient.get(url, T163User.class);
+	}
 
 }
---
+++ src/test/java/com/t163/api/T163UsersTest.java
@@ -0,0 +1,37 @@
+/*
+ * @(#)T163UsersTest.java $version 2013年12月21日
+ *
+ * Copyright 2013 DaLian Software. All rights Reserved.
+ * DaLian Software PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.t163.api;
+
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+
+import com.t163.api.test.AbstractTest;
+
+/**
+ * DaLian Software t-163-api
+ * com.t163.api.T163UsersTest.java
+ * @author cuizuoli
+ * @date 2013年12月21日
+ */
+public class T163UsersTest extends AbstractTest {
+
+	@Resource
+	private Map<String, String> dataMap;
+
+	@Resource
+	private T163Users t163Users;
+
+	@Test
+	public void show() {
+		t163Users.show(dataMap.get("userId"), dataMap.get("accessToken"));
+	}
+
+}
